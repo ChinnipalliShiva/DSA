9f37acfd6aab4a6469048e6b593fccdd9ccf69fa
Chinnipalli Shiva <chinnipallishiva26@gmail.com>
2025-04-29 08:14:15 +0530

    fix

--- DSA/dataTypes/stack/stack.js
+++
@@ -1,6 +0,0 @@
-//stack - seq collection of elements with LIFO principle
-//stack is abstract datatype
-//stack is a list but mostly differentiated on its behaviour
-//supports push and pop(remove recent element)
-
-//example - browser history tracking,undo operation,call stack in JS runtime
--- DSA/recurssion/sumOfDigits.js
+++
@@ -1,8 +0,0 @@
-function sumOfDigits(n, sum = 0) {
-  if (n === 0) {
-    return sum;
-  }
-  sum = sum + (n % 10);
-  return sumOfDigits(Math.floor(n / 10), sum);
-}
-console.log(sumOfDigits(123));
